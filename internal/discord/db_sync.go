41c7f3dca6c1ed93c6b5da147e0d5a86be633f7b
dreamscached <dreamscache.d@gmail.com>
2021-11-18 20:30:53 +0100

    Remove maybeCreatePostWithReactions (now using maybeCreatePost)

--- internal/discord/db_sync.go
+++ internal/discord/db_sync.go
@@ -60,7 +60,7 @@ func (d *Discord) maybeSyncChannels() {
 
 				for _, m := range msg {
 					m.GuildID = chanObj.GuildID
-					d.maybeCreatePostWithReactions(m, false)
+					d.maybeCreatePost(m, false)
 				}
 
 				beforeID = msg[len(msg)-1].ID
@@ -74,95 +74,6 @@ func (d *Discord) maybeSyncChannels() {
 }
 
 func (d *Discord) maybeCreatePost(m *discordgo.Message, awaitEmbed bool) {
-	eg := entity.NewGuildFromSnowflakeID(m.GuildID)
-	if !d.config.guilds.Contains(eg.DiscordID) {
-		d.logger.Sugar().Debugf("Ignoring message %s from ignored guild %s.", m.ID, m.GuildID)
-		return
-	}
-
-	ec := entity.NewChannelFromSnowflakeID(m.ChannelID, 0)
-	if !d.config.chans.Contains(ec.DiscordID) {
-		d.logger.Sugar().Debugf("Ignoring message %s from ignored channel %s.", m.ID, m.ChannelID)
-		return
-	}
-
-	if d.config.ignoreRegexp.MatchString(m.Content) {
-		d.logger.Sugar().Debugf("Ignoring message %s because it matches ignore regexp.", m.ID)
-		return
-	}
-
-	if len(m.Attachments) == 0 && len(m.Embeds) == 0 {
-		if awaitEmbed {
-			d.logger.Sugar().Debugf("Scheduled attachmentless/embedless message %s for possible embed addition edit.", m.ID)
-			d.awaitEmbedEdit(m)
-		} else {
-			d.logger.Sugar().Debugf("Skipping attachmentless/embedless message %s.", m.ID)
-		}
-		return
-	}
-
-	eu := entity.NewUserFromSnowflakeID(m.Author.ID)
-	ep := entity.NewPostFromSnowflakeID(m.ID, 0, 0, m.Content)
-
-	d.logger.Sugar().Debugf("Creating post for message %s.", m.ID)
-	if err := d.storage.Begin(d.ctx, func(tx pgx.Tx) error {
-		d.logger.Sugar().Debugf("Creating guild ID %s.", m.GuildID)
-		if err := entity.FindOrCreateGuild(d.ctx, tx, eg); err != nil {
-			return err
-		}
-
-		d.logger.Sugar().Debugf("Creating channel ID %s.", m.ChannelID)
-		ec.GuildID = eg.ID
-		if err := entity.FindOrCreateChannel(d.ctx, tx, ec); err != nil {
-			return err
-		}
-
-		d.logger.Sugar().Debugf("Creating user ID %s.", m.Author.ID)
-		if err := entity.FindOrCreateUser(d.ctx, tx, eu); err != nil {
-			return err
-		}
-
-		d.logger.Sugar().Debugf("Creating post ID %s.", m.ID)
-		ep.ChannelID, ep.UserID = ec.ID, eu.ID
-		if err := entity.CreatePost(d.ctx, tx, ep); err != nil {
-			return err
-		}
-
-		d.logger.Sugar().Debugf("Creating attachments for post %s.", m.ID)
-		for i, at := range m.Attachments {
-			if at.Width != 0 && at.Height != 0 {
-				im := entity.NewImageFromAttachment(at, ep.ID)
-				if err := entity.CreateImage(d.ctx, tx, im); err != nil {
-					return err
-				}
-			} else {
-				d.logger.Sugar().Debugf("Ignoring non-image attachment #%d %s.", i, at.ID)
-			}
-		}
-		for i, em := range m.Embeds {
-			if em.Image != nil {
-				d.logger.Sugar().Debugf("Creating image struct from embed #%d.", i)
-				im, err := entity.NewImageFromEmbed(d.ctx, em, ep.ID)
-				if err != nil {
-					return err
-				}
-				if err := entity.CreateImage(d.ctx, tx, im); err != nil {
-					return err
-				}
-			} else {
-				d.logger.Sugar().Debugf("Ignoring non-image embed #%d.", i)
-			}
-		}
-
-		return nil
-	}); err != nil {
-		d.logger.Sugar().Errorf("Failed to complete post creation transaction: %s.", err)
-	} else {
-		d.logger.Sugar().Infof("Finished creating post for message %s.", m.ID)
-	}
-}
-
-func (d *Discord) maybeCreatePostWithReactions(m *discordgo.Message, awaitEmbed bool) {
 	eg := entity.NewGuildFromSnowflakeID(m.GuildID)
 	if !d.config.guilds.Contains(eg.DiscordID) {
 		d.logger.Sugar().Debugf("Ignoring message %s from ignored guild %s.", m.ID, m.GuildID)
